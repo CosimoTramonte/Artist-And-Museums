e08e7077ab8d442c19ff9bc62f1310362b015d0d
GiacomoArena <giacomoarena28@gmail.com>
2023-06-14 17:33:05 +0200

    make yeld

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header>
+
+    <ul class="nav">
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Artist</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Museum</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="{{route('home')}}">Home</a>
+        </li>
+    </ul>
+
+</header>
